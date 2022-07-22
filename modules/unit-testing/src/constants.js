8b05dab821a31c1d21f7a9613ec51dd189a0d4e3
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-22 08:29:28 +0200

    feat: create method of convert csv  to json

---
+++ modules/unit-testing/src/constants.js
@@ -0,0 +1,9 @@
+const constants = {
+  error:{ 
+    FILE_LENGTH_ERROR_MESSAGE: 'The content length is invalid!',
+    FILE_FIELDS_ERROR_MESSAGE: 'The provided property are invalids'
+  }
+}
+
+
+module.exports = constants
---
+++ modules/unit-testing/src/file.js
@@ -0,0 +1,64 @@
+const { readFile } = require("fs/promises");
+
+const { error } = require("./constants");
+const User = require("./user");
+
+const default_options = {
+  maxLines: 3,
+  fields: ["id", "name", "profession", "age"],
+};
+
+class File {
+  static async csvToJson(filepath) {
+    const content = await this.getFilePath(filepath);
+    const validation = File.isValid(content);
+    if (!validation.valid) throw new Error(validation.error);
+    const users = File.parseCsvtoJson(content);
+
+    return users;
+  }
+
+  static async getFilePath(filepath) {
+    return (await readFile(filepath)).toString("utf8");
+  }
+  static isValid(csvString, options = default_options) {
+    const [header, ...fileWithoutHeader] = csvString.split("\n");
+    const isHeaderValid = header === options.fields.join(",");
+    if (!isHeaderValid) {
+      return {
+        error: error.FILE_LENGTH_ERROR_MESSAGE,
+        valid: false,
+      };
+    }
+
+    const isContentLengthAccepted =
+      fileWithoutHeader.length > 0 &&
+      fileWithoutHeader.length <= options.maxLines;
+
+    if (!isContentLengthAccepted) {
+      return {
+        error: error.FILE_FIELDS_ERROR_MESSAGE,
+        valid: false,
+      };
+    }
+
+    return { valid: true };
+  }
+
+  static parseCsvtoJson(csvString) {
+    const lines = csvString.split("\n");
+    const firstLine = lines.shift();
+    const header = firstLine.split(",");
+    const users = lines.map((line) => {
+      const columns = line.split(",");
+      let user = {};
+      for (const index in columns) {
+        user[header[index]] = columns[index];
+      }
+      return new User(user);
+    });
+    return users;
+  }
+}
+
+module.exports = File;
