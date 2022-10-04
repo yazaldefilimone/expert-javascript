415a306ca2b5f4576bb4ff63eab3d8dc5e013f4c
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-10-04 07:09:17 +0200

    feat: learning variables

---
+++ beginner/variables.js
@@ -0,0 +1,12 @@
+console.log("piWithVar: ", piWithVar);
+var piWithVar = 3.141592;
+console.log("piWithVar: ", piWithVar);
+
+// let
+
+// console.log("piWithLet", piWithLet); // Error
+let piWithLet = 3.141592;
+console.log("piWithLet", piWithLet);
+
+const piWithConst = 3.141592;
+console.log("piWithConst", piWithConst);
