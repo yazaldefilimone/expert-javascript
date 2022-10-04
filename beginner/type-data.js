6a3c9a78dbc702910846f802a27ce360029d0eca
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-10-04 07:18:08 +0200

    feat: learning data type in javascript

---
+++ beginner/type-data.js
@@ -0,0 +1,13 @@
+const number = 10;
+const string = "Javascript";
+const boolean = true;
+const symbol = Symbol("iterator");
+const object = null;
+const undefin = undefined;
+
+console.log(number, ":", typeof number);
+console.log(string, ":", typeof string);
+console.log(boolean, ":", typeof boolean);
+console.log(symbol, ":", typeof symbol);
+console.log(object, ":", typeof object);
+console.log(undefined, ":", typeof undefin);
