2285f69ae72e2ff9a5455b5a7fec4bfb9b58566c
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-10-04 07:27:38 +0200

    learn: numeric operators

---
+++ beginner/numeric-operators.js
@@ -0,0 +1,22 @@
+console.log(2 + 2);
+console.log(2 - 2);
+console.log(2 * 2);
+console.log(2 / 2);
+console.log(7 % 2);
+console.log("_________________________");
+let result = 10;
+console.log((result += 2));
+console.log((result -= 2));
+console.log((result *= 2));
+console.log((result /= 2));
+console.log((result *= 2));
+console.log("+++++++++++++++++++++++++");
+let operator = 10;
+console.log(operator++);
+console.log(operator);
+console.log(++operator);
+console.log("_________________________");
+
+console.log(operator--);
+console.log(operator);
+console.log(--operator);
